3f859965cc895f4fe6b7d20caebf96feb59302db
xuxiao-666 <1510026680@qq.com>
2025-02-28 12:51:00 +0800

    saletest-dev1

--- yudao-module-erp/yudao-module-erp-biz/src/main/java/cn/iocoder/yudao/module/erp/controller/admin/sale/SaleTest1.java
+++ yudao-module-erp/yudao-module-erp-biz/src/main/java/cn/iocoder/yudao/module/erp/controller/admin/sale/SaleTest1.java
@@ -3,5 +3,6 @@ package cn.iocoder.yudao.module.erp.controller.admin.sale;
 public class SaleTest1 {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("Hello World!");
     }
 }
