f27aa30cb00423dc2c5201579318ac6d5ab1146e
xuxiao-666 <1510026680@qq.com>
2025-02-28 14:10:04 +0800

    test-dev1

---
+++ yudao-module-erp/yudao-module-erp-biz/src/main/java/cn/iocoder/yudao/module/erp/controller/admin/sale/Test1.java
@@ -0,0 +1,8 @@
+package cn.iocoder.yudao.module.erp.controller.admin.sale;
+
+public class Test1 {
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World!");
+    }
+}
